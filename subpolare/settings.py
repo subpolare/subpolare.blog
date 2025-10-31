331e4d8bb7f42a8a9de74332d37aa8b743a590e5
Влад Начатой <subpolare@MacBook-Air-2.local>
2025-10-31 13:24:21 +0300

    fix: fix some bags in Caddyfile

--- subpolare/settings.py
+++ subpolare/settings.py
@@ -180,14 +180,13 @@ DOMAIN_LANGUAGE_SELECTOR = [
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = "/app/static"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "/app/media"
 
 STATICFILES_DIRS = [
     BASE_DIR / "frontend/static",
 ]
-
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
