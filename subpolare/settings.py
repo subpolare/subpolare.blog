ff17ba43784bcd16e99e6ccd91c02dff0e72d01c
Влад Начатой <subpolare@MacBook-Air-2.local>
2025-11-01 14:25:47 +0300

    feat: remove certbot changes in nginx config

--- subpolare/settings.py
+++ subpolare/settings.py
@@ -179,10 +179,9 @@ DOMAIN_LANGUAGE_SELECTOR = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = "/app/static"
-MEDIA_URL = "/media/"
-MEDIA_ROOT = "/app/media"
+STATIC_URL = "static/"
+
+# STATIC_ROOT = BASE_DIR / "frontend/test"
 
 STATICFILES_DIRS = [
     BASE_DIR / "frontend/static",
