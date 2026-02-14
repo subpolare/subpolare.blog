021e2acf0a72416d006c7e75ceb6ff58474970d9
Влад Начатой <subpolare@MacBook-Air-2.local>
2026-02-14 21:28:55 +0300

    fix: fix spoiler blocks in Safari

--- subpolare/settings.py
+++ subpolare/settings.py
@@ -179,7 +179,7 @@ DOMAIN_LANGUAGE_SELECTOR = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "tmp/static"
 
 STATICFILES_DIRS = [
